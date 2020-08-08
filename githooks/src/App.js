923868b677c2979788eadd8ad9c244755739bf84
Lucas362 <lucasvitorifg@gmail.com>
2020-08-08 11:44:04 -0300

    Using state hook

--- githooks/src/App.js
+++ githooks/src/App.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  return <h1>Hello world</h1>;
+  const [repositories, setRepositories] = useState([
+    { id: 1, name: 'repo-1'},
+    { id: 2, name: 'repo-2'},
+    { id: 3, name: 'repo-3'},
+  ]);
+
+  function handleAddRepository() {
+    setRepositories([
+      ...repositories,
+      {id: Math.random(), name: 'Novo repo'}
+    ]);
+  }
+
+  return (
+    <>
+      <ul>
+        {
+          repositories.map(repo => (
+            <li key={repo.id}>{repo.name}</li>
+          ))
+        }
+      </ul>
+      <button onClick={handleAddRepository}>
+        Adicionar repositório
+      </button>
+    </>
+  );
 }
